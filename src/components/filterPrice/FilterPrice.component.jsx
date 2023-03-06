7b49f2211a72ac2db1e30cca1fa220cc0105666f
masoud <masoud30376@gmail.com>
2023-03-06 23:31:50 +0330

    max and min price filtere added

--- src/components/filterPrice/FilterPrice.component.jsx
+++ src/components/filterPrice/FilterPrice.component.jsx
@@ -1,10 +1,13 @@
 import st from 'assets/styles/pages/products.pages.module.scss'
 import { useState } from 'react';
 
-const FilterPrice = () => {
+const FilterPrice = (props) => {
 
+	const changeHandler =(e)=>{
+		props.setFilterInfo(e.target.value)
+		console.log(e.target.value);
+	}
    const [price , setPrice] = useState(100000)
-
    const handleInput = (e) => {
 		setPrice(e.target.value);
    };
@@ -14,57 +17,77 @@ const FilterPrice = () => {
 		{ name: "C", price: 60 },
    ];
    return (
-		// <div className="App">
-		// 	<input type="range" onInput={handleInput} />
-		// 	<h1>Price: {price}</h1>
-		// 	<div>
-		// 		{hotels
-		// 			.filter((hotel) => {
-		// 				return hotel.price > parseInt(price, 1000);
-		// 			})
-		// 			.map((hotel) => {
-		// 				return (
-		// 					<p key={hotel.name}>
-		// 						{hotel.name} | {hotel.price} &euro;{" "}
-		// 					</p>
-		// 				);
-		// 			})}
-		// 	</div>
-		// </div>
-   // );
+		<>
 		<section className={st.filterSection}>
 			<h2 className={st.filterTitle}>{` فیلتر ها`}</h2>
 			<div className={st.line} />
 			<div className={st.filterBody}>
 				<div>
-					<input type="radio" name="filter" value="بیشترین" id="" />
-					<label htmlFor="">بیشترین</label>
+					<input 
+						type="radio" 
+						name="filter"
+						checked={props.filterInfo==="max"}
+						onChange={changeHandler}
+						value="max" 
+						id="" />
+					<label htmlFor="">بیشترین قیمت</label>
 				</div>
 				<div>
-					<input type="radio" name="filter" value="کم ترین " id="" />
-					<label htmlFor="">کم ترین </label>
+					<input 
+						type="radio" 
+						name="filter" 
+						value="min" 
+						checked={props.filterInfo==="min"}
+						onChange={changeHandler}
+						id="" />
+					<label htmlFor="">کم ترین  قیمت</label>
 				</div>
 				<div>
 					<input
 						type="radio"
 						name="filter"
-						value="بیشترین تخفیف"
+						checked={props.filterInfo==="available"}
+						onChange={changeHandler}
+						value="available"
 						id=""
 					/>
-					<label htmlFor="">بیشترین تخفیف</label>
+					<label htmlFor=""> کالا های موجود</label>
 				</div>
-				<div>
+				{/* <div>
 					<input
 						type="radio"
 						name="filter"
-						value="پرفروش ترین"
+						value="popular"
+						checked={props.filterInfo==="popular"}
+						onChange={changeHandler}
 						id=""
 					/>
 					<label htmlFor="">پرفروش ترین</label>
-				</div>
+				</div> */}
 			</div>
 			<div className={st.line} />
 		</section>
+
+
+		{/* <div className="App">
+			<input type="range" onInput={handleInput} />
+			<h1>Price: {price}</h1>
+			<div>
+				{hotels
+					.filter((hotel) => {
+						return hotel.price > parseInt(price, 1000);
+					})
+					.map((hotel) => {
+						return (
+							<p key={hotel.name}>
+								{hotel.name} | {hotel.price} &euro;{" "}
+							</p>
+						);
+					})}
+			</div>
+		</div> */}
+		</>
+		
    );
 }
 export  {FilterPrice}
--- src/pages/products/products.page.jsx
+++ src/pages/products/products.page.jsx
@@ -1,6 +1,7 @@
 import { PageContainer } from 'layouts'
 import { Fragment } from 'react'
 import st from 'assets/styles/pages/products.pages.module.scss'
+import st2 from 'assets/styles/components/product.component.module.scss'
 import SingleProduct from 'pages/singlePageProduct/singleProduct.page'
 import { FilterPrice, Loading, Pagination, Product, SpecialProduct } from 'components'
 import { useSelector } from 'react-redux'
@@ -8,23 +9,44 @@ import { logDOM } from '@testing-library/react'
 import { useState  , useEffect} from 'react'
 import { useAllProductDataQuery } from 'apis'
 import { useParams } from 'react-router-dom'
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { INTERNAL_PATHS } from 'configs/routes.config'
+import { singleProductAction } from 'store'
 
 
 const Products = () => {
    const theTitle = useSelector((state)=>state.menuTitle.menuTitle)
    const [currentPage, setCurrentPage] = useState(1);
-   const [postsPerPage] = useState(3);
+   const [postsPerPage] = useState(6);
    const {data:fetchedData ,isLoading} = useAllProductDataQuery()
-
-
    const category = useParams()
-   console.log(category.productName)
-   console.log(fetchedData)
    let filteredData = ""
+   // product comp
+	const navigate = useNavigate()
+	const dispatch = useDispatch()
+   const [filterInfo,setFilterInfo] = useState(false)
+   console.log(filterInfo);
 
+   /*********************************** Functions ***************************************/
+
+   const addToBasketHandler = (e)=>{
+		e.stopPropagation()
+		console.log(e.target.parentElement.parentElement);
+	}
+   const handleCartClick =(id)=>{
+		console.log(id)
+		//1 here pass data to store and then to singleCart  page
+		dispatch(singleProductAction.moveProduct({id}))
+		//2 navigate to single page product
+		navigate(INTERNAL_PATHS.SINGLE_PRODUCT_PAGE)
+	}
+   
+   
+   /****************************** pagination variables **********************************/
    if(isLoading){
       return <Loading/>
-   }
+   } 
 
    switch (category.productName) {
 		case 'mouse':
@@ -66,20 +88,42 @@ const Products = () => {
       default: console.log("nothing matched");
    }
    
-
-   // pagination part 
    const indexOfLastPost = currentPage * postsPerPage
    const indexOfFirstPost = indexOfLastPost - postsPerPage
    const currentPosts = filteredData.slice(indexOfFirstPost,indexOfLastPost)
-
    const paginate = (pageNumber)=>{setCurrentPage(pageNumber)} 
+   console.log(currentPosts);
 
-   // console.log(indexOfFirstPost, indexOfLastPost , currentPosts);
+   let filterers
+
+   if(filterInfo==="max"){
+      filterers = currentPosts
+      console.log(filterers , "before");
+      filterers = filterers.sort((a,b)=> {  
+         if(a.price>b.price){
+            return -1
+         }
+      })
+      console.log(filterers ,'after');
+   }else if (filterInfo==="min"){
+      filterers=currentPosts
+      filterers = filterers.sort((a,b)=> {  
+         if(a.price<b.price){
+            return -1
+         }
+      })
+      console.log(filterers ,'after');
+   }else if(filterInfo==="available"){
+      console.log("available");
+      filterers=[]
+   }
    
-
    return (
       <Fragment>
          <PageContainer>
+
+            {/* ************************* Banners ************************* */}
+
             <div className={` ${st.landingBanner}`}>
                <div className={st._1stBanner}>
                   <img src="https://www.technolife.ir/image/banner_CenterTwinBanners_npIrqU_55404ba1-ae85-4d65-9cd8-36911406d48a.png" alt=" " />
@@ -88,11 +132,14 @@ const Products = () => {
                   <img src="https://www.technolife.ir/image/banner_CenterTwinBanners_tqGh8a_d0eaa460-6ea6-457a-8ea0-801f6faca1da.png" alt=" " />
                </div>
             </div>
+
+            {/* ************************* Filters ************************* */}
+
             <div className={st.wrapper}>
                <div className={st.filters}>
-                  <FilterPrice/>
-                  <FilterPrice/>
-                  <FilterPrice/>
+                  <FilterPrice 
+                     setFilterInfo={setFilterInfo} 
+                     filterInfo={filterInfo}/>
                   {/* <section className={st.filterSection}>
                      <h2 className={st.filterTitle}>{` برند ها `}</h2>
                      <div className={st.line}/>
@@ -119,12 +166,80 @@ const Products = () => {
                   </section> */}
                </div>
 
+            {/* ************************* Products ************************* */}
+
                <SpecialProduct title = {theTitle.title}>
-                  <Product posts={currentPosts}/>
+                  {/* <Product posts={currentPosts}/> */}
+                  
+                  {filterInfo===false ? 
+                     currentPosts.map(({name, id , price, image}) =>(
+                        <div className={st2.products} key={id} onClick={()=>{handleCartClick(id)}}>
+                           <div className={st2.productImage}>
+                              <img 	
+                                 src={image} 
+                                 alt=" " />
+                           </div>
+                           <div className={st2.productContent}>
+                              <span>{name || "error"}</span>
+                              <div className={st2.price}>
+                                 <button onClick={(e)=>addToBasketHandler(e)}>
+                                    <svg
+                                       xmlns="http://www.w3.org/2000/svg"
+                                       width="30"
+                                       height="30"
+                                       fill="currentColor"
+                                       class="bi bi-bag-fill"
+                                       viewBox="0 0 16 16"
+                                    >
+                                       <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5z" />
+                                    </svg>
+                                 </button>
+                                 <span>
+                                    {price || 0}
+                                    <small> تومان</small>
+                                 </span>
+                              </div>
+                           </div>
+                        </div>
+                        )
+                     ): 
+                     filterers.map(({name, id , price, image}) =>(
+                        <div className={st2.products} key={id} onClick={()=>{handleCartClick(id)}}>
+                           <div className={st2.productImage}>
+                              <img 	
+                                 src={image} 
+                                 alt=" " />
+                           </div>
+                           <div className={st2.productContent}>
+                              <span>{name || "error"}</span>
+                              <div className={st2.price}>
+                                 <button onClick={(e)=>addToBasketHandler(e)}>
+                                    <svg
+                                       xmlns="http://www.w3.org/2000/svg"
+                                       width="30"
+                                       height="30"
+                                       fill="currentColor"
+                                       class="bi bi-bag-fill"
+                                       viewBox="0 0 16 16"
+                                    >
+                                       <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5z" />
+                                    </svg>
+                                 </button>
+                                 <span>
+                                    {price || 0}
+                                    <small> تومان</small>
+                                 </span>
+                              </div>
+                           </div>
+                        </div>
+                        )
+                     )
+                  }
+
                </SpecialProduct>
             </div>
 
-            {/* pagination */}
+            {/* ************************* Pagination ************************* */}
             <div>
                <Pagination 
                   postPerPage={postsPerPage} 
