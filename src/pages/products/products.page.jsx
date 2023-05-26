01312622a3f336ce062079ad0a3fdfd5379dfd47
masoud <masoud30376@gmail.com>
2023-05-26 13:55:27 +0430

    test commit

--- src/pages/products/products.page.jsx
+++ src/pages/products/products.page.jsx
@@ -1,151 +1,164 @@
-import { PageContainer } from 'layouts'
-import { Fragment } from 'react'
-import st from 'assets/styles/pages/products.pages.module.scss'
-import st2 from 'assets/styles/components/product.component.module.scss'
-import SingleProduct from 'pages/singlePageProduct/singleProduct.page'
-import { FilterPrice, Loading, Pagination, Product, SpecialProduct } from 'components'
-import { useSelector } from 'react-redux'
-import { logDOM } from '@testing-library/react'
-import { useState  , useEffect} from 'react'
-import { useAllProductDataQuery } from 'apis'
-import { useParams } from 'react-router-dom'
-import { useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import { INTERNAL_PATHS } from 'configs/routes.config'
-import { singleProductAction } from 'store'
-import { convertNumberToPersian } from 'utils'
-
+import { PageContainer } from "layouts";
+import { Fragment } from "react";
+import st from "assets/styles/pages/products.pages.module.scss";
+import st2 from "assets/styles/components/product.component.module.scss";
+import SingleProduct from "pages/singlePageProduct/singleProduct.page";
+import {
+	FilterPrice,
+	Loading,
+	Pagination,
+	Product,
+	SpecialProduct,
+} from "components";
+import { useSelector } from "react-redux";
+import { logDOM } from "@testing-library/react";
+import { useState, useEffect } from "react";
+import { useAllProductDataQuery } from "apis";
+import { useParams } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { INTERNAL_PATHS } from "configs/routes.config";
+import { singleProductAction } from "store";
+import { convertNumberToPersian } from "utils";
 
 const Products = () => {
-   const theTitle = useSelector((state)=>state.menuTitle.menuTitle)
-   const [currentPage, setCurrentPage] = useState(1);
-   const [postsPerPage] = useState(6);
-   const {data:fetchedData ,isLoading} = useAllProductDataQuery()
-   const category = useParams()
-   let filteredData = ""
-   // product comp
-	const navigate = useNavigate()
-	const dispatch = useDispatch()
-   const [filterInfo,setFilterInfo] = useState(false)
-   console.log(filterInfo);
+	const theTitle = useSelector((state) => state.menuTitle.menuTitle);
+	const [currentPage, setCurrentPage] = useState(1);
+	const [postsPerPage] = useState(6);
+	const { data: fetchedData, isLoading } = useAllProductDataQuery();
+	const category = useParams();
+	let filteredData = "";
+	// product comp
+	const navigate = useNavigate();
+	const dispatch = useDispatch();
+	const [filterInfo, setFilterInfo] = useState(false);
+	console.log(filterInfo);
 
-   /*********************************** Functions ***************************************/
+	/*********************************** Functions ***************************************/
 
-   const addToBasketHandler = (e)=>{
-		e.stopPropagation()
+	const addToBasketHandler = (e) => {
+		e.stopPropagation();
 		console.log(e.target.parentElement.parentElement);
-	}
-   const handleCartClick =(id)=>{
-		console.log(id)
+	};
+	const handleCartClick = (id) => {
+		console.log(id);
 		//1 here pass data to store and then to singleCart  page
-		dispatch(singleProductAction.moveProduct({id}))
+		dispatch(singleProductAction.moveProduct({ id }));
 		//2 navigate to single page product
-		navigate(INTERNAL_PATHS.SINGLE_PRODUCT_PAGE)
+		navigate(INTERNAL_PATHS.SINGLE_PRODUCT_PAGE);
+	};
+
+	/****************************** pagination variables **********************************/
+	if (isLoading) {
+		return <Loading />;
 	}
-   
-   
-   /****************************** pagination variables **********************************/
-   if(isLoading){
-      return <Loading/>
-   } 
 
-   switch (category.productName) {
-		case 'mouse':
-			filteredData = fetchedData.filter((val)=>val.category === 1)
-         // console.log(filteredData)
+	switch (category.productName) {
+		case "mouse":
+			filteredData = fetchedData.filter((val) => val.category === 1);
+			// console.log(filteredData)
 			break;
-		case  "pc":
-			filteredData = fetchedData.filter((val)=>val.category === 2)
+		case "pc":
+			filteredData = fetchedData.filter((val) => val.category === 2);
 			// console.log(filteredData);
 			break;
-      case  "graphic-cart":
-         filteredData = fetchedData.filter((val)=>val.category === 3)
-         // console.log(filteredData);
-         break;
-		case  "monitor":
-			filteredData = fetchedData.filter((val)=>val.category === 4)
+		case "graphic-cart":
+			filteredData = fetchedData.filter((val) => val.category === 3);
 			// console.log(filteredData);
 			break;
-      case  "keyboard":
-         filteredData = fetchedData.filter((val)=>val.category === 5)
-         // console.log(filteredData);
-         break;
-		case  "accessories":
-			filteredData = fetchedData.filter((val)=>val.category === 6)
+		case "monitor":
+			filteredData = fetchedData.filter((val) => val.category === 4);
 			// console.log(filteredData);
 			break;
-		case  "mobile":
-			filteredData = fetchedData.filter((val)=>val.category === 7)
+		case "keyboard":
+			filteredData = fetchedData.filter((val) => val.category === 5);
 			// console.log(filteredData);
 			break;
-		case  "handsfree":
-			filteredData = fetchedData.filter((val)=>val.category === 8)
+		case "accessories":
+			filteredData = fetchedData.filter((val) => val.category === 6);
 			// console.log(filteredData);
 			break;
-		case  "headphone":
-			filteredData = fetchedData.filter((val)=>val.category === 9)
+		case "mobile":
+			filteredData = fetchedData.filter((val) => val.category === 7);
 			// console.log(filteredData);
 			break;
-      default: console.log("nothing matched");
-   }
-   
-   const indexOfLastPost = currentPage * postsPerPage
-   const indexOfFirstPost = indexOfLastPost - postsPerPage
-   const currentPosts = filteredData.slice(indexOfFirstPost,indexOfLastPost)
-   const paginate = (pageNumber)=>{setCurrentPage(pageNumber)} 
-   // console.log(currentPosts);
+		case "handsfree":
+			filteredData = fetchedData.filter((val) => val.category === 8);
+			// console.log(filteredData);
+			break;
+		case "headphone":
+			filteredData = fetchedData.filter((val) => val.category === 9);
+			// console.log(filteredData);
+			break;
+		default:
+			console.log("nothing matched");
+	}
 
-   let filterers
+	const indexOfLastPost = currentPage * postsPerPage;
+	const indexOfFirstPost = indexOfLastPost - postsPerPage;
+	const currentPosts = filteredData.slice(indexOfFirstPost, indexOfLastPost);
+	const paginate = (pageNumber) => {
+		setCurrentPage(pageNumber);
+	};
+	// console.log(currentPosts);
 
-   if(filterInfo==="max"){
-      filterers = currentPosts
-      console.log(filterers , "before");
-      filterers = filterers.sort((a,b)=> {  
-         if(a.price>b.price){
-            return -1
-         }
-      })
-      console.log(filterers ,'after');
-   }else if (filterInfo==="min"){
-      filterers=currentPosts
-      filterers = filterers.sort((a,b)=> {  
-         if(a.price<b.price){
-            return -1
-         }
-      })
-      console.log(filterers ,'after');
-   }else if(filterInfo==="available"){
-      console.log("available");
-      filterers=[]
-   }else if(filterInfo==="noFilter"){
-      console.log(currentPosts);
-      filterers=currentPosts
-      console.log(filterers , "nooooooooooooo filter");
-   }
-   
-   return (
-      <Fragment>
-         <PageContainer>
+	let filterers;
 
-            {/* ************************* Banners ************************* */}
+	if (filterInfo === "max") {
+		filterers = currentPosts;
+		console.log(filterers, "before");
+		filterers = filterers.sort((a, b) => {
+			if (a.price > b.price) {
+				return -1;
+			}
+		});
+		console.log(filterers, "after");
+	} else if (filterInfo === "min") {
+		filterers = currentPosts;
+		filterers = filterers.sort((a, b) => {
+			if (a.price < b.price) {
+				return -1;
+			}
+		});
+		console.log(filterers, "after");
+	} else if (filterInfo === "available") {
+		console.log("available");
+		filterers = [];
+	} else if (filterInfo === "noFilter") {
+		console.log(currentPosts);
+		filterers = currentPosts;
+		console.log(filterers, "nooooooooooooo filter");
+	}
 
-            <div className={` ${st.landingBanner}`}>
-               <div className={st._1stBanner}>
-                  <img src="https://www.technolife.ir/image/banner_CenterTwinBanners_npIrqU_55404ba1-ae85-4d65-9cd8-36911406d48a.png" alt=" " />
-               </div>
-               <div className={st._2ndBanner}>
-                  <img src="https://www.technolife.ir/image/banner_CenterTwinBanners_tqGh8a_d0eaa460-6ea6-457a-8ea0-801f6faca1da.png" alt=" " />
-               </div>
-            </div>
+	return (
+		<Fragment>
+			<PageContainer>
+				{/* ************************* Banners ************************* */}
 
-            {/* ************************* Filters ************************* */}
+				<div className={` ${st.landingBanner}`}>
+					<div className={st._1stBanner}>
+						<img
+							src="https://www.technolife.ir/image/banner_CenterTwinBanners_npIrqU_55404ba1-ae85-4d65-9cd8-36911406d48a.png"
+							alt=" "
+						/>
+					</div>
+					<div className={st._2ndBanner}>
+						<img
+							src="https://www.technolife.ir/image/banner_CenterTwinBanners_tqGh8a_d0eaa460-6ea6-457a-8ea0-801f6faca1da.png"
+							alt=" "
+						/>
+					</div>
+				</div>
 
-            <div className={st.wrapper}>
-               <div className={st.filters}>
-                  <FilterPrice 
-                     setFilterInfo={setFilterInfo} 
-                     filterInfo={filterInfo}/>
-                  {/* <section className={st.filterSection}>
+				{/* ************************* Filters ************************* */}
+
+				<div className={st.wrapper}>
+					<div className={st.filters}>
+						<FilterPrice
+							setFilterInfo={setFilterInfo}
+							filterInfo={filterInfo}
+						/>
+						{/* <section className={st.filterSection}>
                      <h2 className={st.filterTitle}>{` برند ها `}</h2>
                      <div className={st.line}/>
                      <div className={st.filterBody}>
@@ -169,93 +182,106 @@ const Products = () => {
                      </div>
                      <div className={st.line}/>
                   </section> */}
-               </div>
+					</div>
 
-            {/* ************************* Products ************************* */}
+					{/* ************************* Products ************************* */}
 
-               <SpecialProduct title = {theTitle.title}>
-                  {/* <Product posts={currentPosts}/> */}
-                  
-                  {filterInfo===false ? 
-                     currentPosts.map(({name, id , price, image}) =>(
-                        <div className={st2.products} key={id} onClick={()=>{handleCartClick(id)}}>
-                           <div className={st2.productImage}>
-                              <img 	
-                                 src={image} 
-                                 alt=" " />
-                           </div>
-                           <div className={st2.productContent}>
-                              <span>{name || "error"}</span>
-                              <div className={st2.price}>
-                                 <button onClick={(e)=>addToBasketHandler(e)}>
-                                    <svg
-                                       xmlns="http://www.w3.org/2000/svg"
-                                       width="30"
-                                       height="30"
-                                       fill="currentColor"
-                                       class="bi bi-bag-fill"
-                                       viewBox="0 0 16 16"
-                                    >
-                                       <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5z" />
-                                    </svg>
-                                 </button>
-                                 <span>
-                                    {convertNumberToPersian(price) || 0}
-                                    <small> تومان</small>
-                                 </span>
-                              </div>
-                           </div>
-                        </div>
-                        )
-                     ): 
-                     filterers.map(({name, id , price, image}) =>(
-                        <div className={st2.products} key={id} onClick={()=>{handleCartClick(id)}}>
-                           <div className={st2.productImage}>
-                              <img 	
-                                 src={image} 
-                                 alt=" " />
-                           </div>
-                           <div className={st2.productContent}>
-                              <span>{name || "error"}</span>
-                              <div className={st2.price}>
-                                 <button onClick={(e)=>addToBasketHandler(e)}>
-                                    <svg
-                                       xmlns="http://www.w3.org/2000/svg"
-                                       width="30"
-                                       height="30"
-                                       fill="currentColor"
-                                       class="bi bi-bag-fill"
-                                       viewBox="0 0 16 16"
-                                    >
-                                       <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5z" />
-                                    </svg>
-                                 </button>
-                                 <span>
-                                    {price || 0}
-                                    <small> تومان</small>
-                                 </span>
-                              </div>
-                           </div>
-                        </div>
-                        )
-                     )
-                  }
+					<SpecialProduct title={theTitle.title}>
+						{/* <Product posts={currentPosts}/> */}
 
-               </SpecialProduct>
-            </div>
+						{filterInfo === false
+							? currentPosts.map(({ name, id, price, image }) => (
+									<div
+										className={st2.products}
+										key={id}
+										onClick={() => {
+											handleCartClick(id);
+										}}
+									>
+										<div className={st2.productImage}>
+											<img src={image} alt=" " />
+										</div>
+										<div className={st2.productContent}>
+											<span>{name || "error"}</span>
+											<div className={st2.price}>
+												<button
+													onClick={(e) =>
+														addToBasketHandler(e)
+													}
+												>
+													<svg
+														xmlns="http://www.w3.org/2000/svg"
+														width="30"
+														height="30"
+														fill="currentColor"
+														class="bi bi-bag-fill"
+														viewBox="0 0 16 16"
+													>
+														<path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5z" />
+													</svg>
+												</button>
+												<span>
+													{convertNumberToPersian(
+														price
+													) || 0}
+													<small> تومان</small>
+												</span>
+											</div>
+										</div>
+									</div>
+							  ))
+							: filterers.map(({ name, id, price, image }) => (
+									<div
+										className={st2.products}
+										key={id}
+										onClick={() => {
+											handleCartClick(id);
+										}}
+									>
+										<div className={st2.productImage}>
+											<img src={image} alt=" " />
+										</div>
+										<div className={st2.productContent}>
+											<span>{name || "error"}</span>
+											<div className={st2.price}>
+												<button
+													onClick={(e) =>
+														addToBasketHandler(e)
+													}
+												>
+													<svg
+														xmlns="http://www.w3.org/2000/svg"
+														width="30"
+														height="30"
+														fill="currentColor"
+														class="bi bi-bag-fill"
+														viewBox="0 0 16 16"
+													>
+														<path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5z" />
+													</svg>
+												</button>
+												<span>
+													{price || 0}
+													<small> تومان</small>
+												</span>
+											</div>
+										</div>
+									</div>
+							  ))}
+					</SpecialProduct>
+				</div>
 
-            {/* ************************* Pagination ************************* */}
-            <div>
-               <Pagination 
-                  postPerPage={postsPerPage} 
-                  totalPost={fetchedData.length} 
-                  paginate={paginate}
-               />
-            </div>
+				{/* ************************* Pagination ************************* */}
+				<div>
+					<Pagination
+						postPerPage={postsPerPage}
+						totalPost={fetchedData.length}
+						paginate={paginate}
+					/>
+				</div>
+			</PageContainer>
+		</Fragment>
+	);
+};
 
-         </PageContainer>
-      </Fragment>
-   )
-}
-
-export default Products
+export default Products;
